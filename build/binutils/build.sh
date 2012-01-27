cf2dc5f60903858d238b8d0dcb0555a221740df3
Eric Sproul <esproul@omniti.com>
2012-01-27 22:23:27 +0000

    tid18365 Update configure defaults.  Add pseudo-URL-encoding for package names.

--- build/binutils/build.sh
+++ build/binutils/build.sh
@@ -8,18 +8,18 @@ export PATH
 CC=/opt/gcc-4.6.2/bin/gcc
 CXX=/opt/gcc-4.6.2/bin/g++
 
-PROG=binutils   # App name
-VER=2.22        # App version
-VERHUMAN=$VER   # Human-readable version
-PVER=1          # Package Version (numeric only)
-PKG=$PROG       # Package name (without prefix)
-SUMMARY="$PROG - Packager was lazy and didn't fill this in" # You should change this
-DESC="$SUMMARY (OmniTI roll)" # Longer description
+PROG=binutils               # App name
+VER=2.22                    # App version
+VERHUMAN=$VER               # Human-readable version
+PVER=1                      # Package Version (numeric only)
+PKG=developer/gnu-binutils  # Package name (without prefix)
+SUMMARY="$PROG -  a collection of binary tools"
+DESC="$SUMMARY"
 
 BUILD_DEPENDS_IPS="gcc46"
 [[ "$BUILDARCH" == "both" ]] && BUILDARCH=32
 
-CONFIGURE_OPTS="--enable-ld=no --enable-gold=no --exec-prefix=/opt/omni/gnu --program-prefix=g"
+CONFIGURE_OPTS="--enable-ld=no --enable-gold=no --exec-prefix=/usr/gnu --program-prefix=g"
 
 init
 download_source $PROG $PROG $VER
