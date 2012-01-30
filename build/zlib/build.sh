ca8cd702c7527680c899fda38259cc86b979ad17
Eric Sproul <esproul@omniti.com>
2012-01-30 20:08:52 +0000

    tid18365 Original zlib build script

---
+++ build/zlib/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/bash
+
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=zlib       # App name
+VER=1.2.5       # App version
+PVER=1          # Package Version
+PKG=$PROG       # Package name (without prefix)
+SUMMARY="$PROG - A massively spiffy yet delicately unobtrusive compression library"
+DESC="$SUMMARY (OmniTI roll)"
+
+CFLAGS="-Kpic"
+
+CONFIGURE_OPTS_32="--prefix=$PREFIX
+    --includedir=$PREFIX/include
+    --libdir=$PREFIX/lib"
+
+CONFIGURE_OPTS_64="--prefix=$PREFIX
+    --includedir=$PREFIX/include/$ISAPART64
+    --libdir=$PREFIX/lib/$ISAPART64"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+fix_permissions
+make_package
+clean_up
