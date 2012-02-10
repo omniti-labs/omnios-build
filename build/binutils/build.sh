b697eb45d03fbec66e40156b94d68c8d7d6ac62e
Eric Sproul <esproul@omniti.com>
2012-02-10 19:49:41 +0000

    tid18365 The illumos build script hardcodes /usr/sfw paths.  We should fix that, but in the mean time make sure those paths are valid.

--- build/binutils/build.sh
+++ build/binutils/build.sh
@@ -6,7 +6,7 @@
 PROG=binutils               # App name
 VER=2.22                    # App version
 VERHUMAN=$VER               # Human-readable version
-PVER=1                      # Package Version (numeric only)
+PVER=2                      # Package Version (numeric only)
 PKG=developer/gnu-binutils  # Package name (without prefix)
 SUMMARY="$PROG -  a collection of binary tools"
 DESC="$SUMMARY"
@@ -16,12 +16,26 @@ BUILD_DEPENDS_IPS="gcc46"
 
 CONFIGURE_OPTS="--enable-ld=no --enable-gold=no --exec-prefix=/usr/gnu --program-prefix=g"
 
+make_sfw_links() {
+    logmsg "Creating SFW symlinks"
+    logcmd mkdir -p $DESTDIR/$PREFIX/sfw/bin
+    pushd $DESTDIR/$PREFIX/sfw/bin > /dev/null
+    for file in gaddr2line gar gas gc++filt gelfedit ggprof gnm gobjcopy \
+                gobjdump granlib greadelf gsize gstrings gstrip
+        do logcmd ln -s ../../bin/$file $file || \
+            logerr "Failed to create link for $file"
+        done
+    popd > /dev/null
+}
+
 init
 download_source $PROG $PROG $VER
 patch_source
 prep_build
 build
 make_isa_stub
+make_sfw_links
 fix_permissions
+logerr "Intentional fail-- check build"
 make_package
 clean_up
