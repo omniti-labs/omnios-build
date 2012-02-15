d9f337cca6a061a52b72b169917530efb0a6316a
Eric Sproul <esproul@omniti.com>
2012-02-15 21:02:11 +0000

    tid18365 Remove /opt/omni paths from libxml2 core build. Bump PVER and remove redundant CC setting as gcc is now default.

--- build/libxml2/build.sh
+++ build/libxml2/build.sh
@@ -3,22 +3,14 @@
 # Load support functions
 . ../../lib/functions.sh
 
-PATH=/opt/gcc-4.6.2/bin:$PATH
-export PATH
-CC=/opt/gcc-4.6.2/bin/gcc
-CXX=/opt/gcc-4.6.2/bin/g++
-
 PROG=libxml2        # App name
 VER=2.7.8           # App version
-PVER=3              # Package Version
+PVER=4              # Package Version
 PKG=library/libxml2 # Package name (without prefix)
 SUMMARY="$PROG - XML C parser and toolkit"
 DESC="$SUMMARY"
 
 DEPENDS_IPS="libgcc_s@4.6.2 library/zlib@1.2.6"
-
-LDFLAGS32="-L/opt/omni/lib -R/opt/omni/lib"
-LDFLAGS64="-L/opt/omni/lib/$ISAPART64 -R/opt/omni/lib/$ISAPART64"
 
 init
 download_source $PROG $PROG $VER
