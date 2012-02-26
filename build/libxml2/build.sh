e059ed9e7e7f736a570c6615529cc16ecf55d36c
Theo Schlossnagle <jesus@omniti.com>
2012-02-26 14:07:08 +0000

    fix python installation paths

--- build/libxml2/build.sh
+++ build/libxml2/build.sh
@@ -5,18 +5,27 @@
 
 PROG=libxml2        # App name
 VER=2.7.8           # App version
-PVER=4              # Package Version
+PVER=4.1            # Package Version
 PKG=library/libxml2 # Package name (without prefix)
 SUMMARY="$PROG - XML C parser and toolkit"
 DESC="$SUMMARY"
 
 DEPENDS_IPS="libgcc_s@4.6.2 library/zlib@1.2.6"
 
+fix_python_install() {
+    logcmd mkdir -p $DESTDIR/usr/lib/python2.6/vendor-packages/64 || logerr "failed mkdir"
+    logcmd mv $DESTDIR/usr/lib/python2.6/site-packages/* $DESTDIR/usr/lib/python2.6/vendor-packages/ || logerr "failed relocating python install"
+    logcmd mv $DESTDIR/usr/lib/amd64/python2.6/site-packages/lib* $DESTDIR/usr/lib/python2.6/vendor-packages/64/ || logerr "failed relocating amd64 python install"
+    logcmd rm -rf $DESTDIR/usr/lib/python2.6/site-packages || logerr "failed removing bad python install"
+    logcmd rm -rf $DESTDIR/usr/lib/amd64/python2.6/site-packages || logerr "failed removing bad amd64 python install"
+}
+
 init
 download_source $PROG $PROG $VER
 patch_source
 prep_build
 build
+fix_python_install
 make_isa_stub
 fix_permissions
 make_package
