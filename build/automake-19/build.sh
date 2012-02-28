411f7509200580a4e2377cf9b670e3c999bb0ec9
Eric Sproul <esproul@omniti.com>
2012-02-28 18:39:50 +0000

    tid18365 automake 1.9 and 1.11.  System default will be 1.11.

---
+++ build/automake-19/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=automake   # App name
+VER=1.9.6       # App version
+VERHUMAN=$VER   # Human-readable version
+PVER=1          # Package Version (numeric only)
+PKG=developer/build/automake-19  # Package name (without prefix)
+SUMMARY="GNU Automake $VER"
+DESC="GNU Automake - A Makefile generator ($VER)"
+
+BUILDARCH=32
+DEPENDS_IPS="developer/macro/gnu-m4 runtime/perl-510"
+
+# Since it's 32-bit only we don't worry about isaexec for bins
+CONFIGURE_OPTS="--bindir=$PREFIX/bin"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+logcmd rm -f $DESTDIR$PREFIX/bin/automake
+logcmd rm -f $DESTDIR$PREFIX/bin/aclocal
+make_isa_stub
+fix_permissions
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
