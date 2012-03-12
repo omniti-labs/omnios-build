d899b512e66c8d4644d225cf64c1139ef4c2cc2b
Theo Schlossnagle <jesus@omniti.com>
2012-03-12 21:08:01 +0000

    cdrtools works

--- build/cdrtools/build.sh
+++ build/cdrtools/build.sh
@@ -13,15 +13,29 @@ DESC="$SUMMARY ($VER)"
 
 DEPENDS_IPS="system/library system/library/gcc-4-runtime"
 
-MAKE="gmake GMAKE_NOWARN=true"
+MAKE="make"
+BUILDARCH=32
 
 # cdrtools doesn't use configure, just make
+make_clean() {
+    true
+}
 configure32() {
     true
 }
-
-configure64() {
-    true
+make_install() {
+    mkdir -p $DESTDIR/etc/security/exec_attr.d
+    mkdir -p $DESTDIR/usr/bin
+    mkdir -p $DESTDIR/usr/share/man/man1
+    cp $SRCDIR/files/exec_attr $DESTDIR/etc/security/exec_attr.d
+    cp $TMPDIR/$BUILDDIR/mkisofs/OBJ/i386-sunos5-gcc/mkisofs $DESTDIR/usr/bin/mkisofs
+    mkdir -p $DESTDIR/usr/share/man/man8
+    cp $TMPDIR/$BUILDDIR/mkisofs/mkisofs.8 $DESTDIR/usr/share/man/man8/mkisofs.8
+    for cmd in cdda2wav cdrecord readcd ; do
+        cp $SRCDIR/files/$cmd $DESTDIR/usr/bin/$cmd
+        cp $TMPDIR/$BUILDDIR/$cmd/OBJ/i386-sunos5-gcc/$cmd $DESTDIR/usr/bin/$cmd.bin
+        cp $TMPDIR/$BUILDDIR/$cmd/$cmd.1 $DESTDIR/usr/share/man/man1/$cmd.1
+    done
 }
 
 init
