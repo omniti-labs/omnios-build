7d7f73be6f77968f4120fc87bc78739e2e0dfdc2
Eric Sproul <esproul@omniti.com>
2012-03-15 21:31:21 +0000

    tid18365 scfdot might come in handy or it might just be busted

---
+++ build/scfdot/build.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=scfdot
+VER=1.0
+VERHUMAN=$VER
+PVER=1
+PKG=system/utility/scfdot
+SUMMARY="scfdot - SMF Dependency Graph Generator"
+DESC="scfdot - Generate a graphviz file of SMF services and dependencies"
+
+DEPENDS_IPS="system/library"
+
+BUILDARCH=32
+
+configure32() {
+    true
+}
+
+make_prog() {
+    logmsg "--- make"
+    logcmd $MAKE CC=$CC scfdot || \
+        logerr "--- Make failed"
+}
+
+make_install() {
+    logmsg "--- make install"
+    logcmd mkdir -p $DESTDIR$PREFIX/bin
+    logcmd cp scfdot $DESTDIR$PREFIX/bin
+    logcmd chmod 0555 $DESTDIR$PREFIX/bin/scfdot
+}
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+fix_permissions
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
