1ef91d0ba144a547a4a99ea921e1b686de0fc858
Eric Sproul <esproul@omniti.com>
2012-03-20 20:56:27 +0000

    tid18365 net-snmp build.  Some patches proved unnecessary with current version, others were only partially obviated.  We do not see a need for 64-bit binaries, so those have been dropped while preserviing 32/64-bit libs.

--- build/net-snmp/build.sh
+++ build/net-snmp/build.sh
@@ -11,14 +11,21 @@ PKG=system/management/snmp/net-snmp
 SUMMARY="Net-SNMP Agent files and libraries"
 DESC="$SUMMARY ($VER)"
 
+NO_PARALLEL_MAKE=true
+
 DEPENDS_IPS="SUNWcs shell/bash system/library
              system/management/snmp/sea/sea-config"
 
 MIB_MODULES="host disman/event-mib ucd-snmp/diskio udp-mib tcp-mib if-mib"
 
+# We want dual-arch libs but only care about 32-bit binaries
+# We will elide 64-bit binaries with pkgmogrify (local.mog)
+CONFIGURE_OPTS_32="$CONFIGURE_OPTS_32 --bindir=$PREFIX/bin --sbindir=$PREFIX/sbin"
 CONFIGURE_OPTS="
     --with-defaults
     --with-default-snmp-version=3
+    --includedir=$PREFIX/include
+    --mandir=$PREFIX/share/man
     --with-logfile=/var/log/snmpd.log
     --with-persistent-directory=/var/net-snmp
     --with-mibdirs=/etc/net-snmp/snmp/mibs
@@ -30,6 +37,7 @@ CONFIGURE_OPTS="
     --with-pkcs
     --disable-embedded-perl
     --without-perl-modules
+    --disable-static
 "
 
 # Options with embedded spaces don't play well w/our functions
@@ -61,13 +69,26 @@ configure64() {
         logerr "--- Configure failed"
 }
 
+# Jam in some SMF files
+place_smf_files() {
+    logmsg "Installing SMF files"
+    for dir in lib/svc/manifest/application/management lib/svc/method; do
+        logcmd mkdir -p $DESTDIR$PREFIX/$dir || \
+            logerr "--- Failed to create directory $DESTDIR$PREFIX/$dir"
+    done
+    logcmd cp $SRCDIR/files/net-snmp.xml $DESTDIR$PREFIX/lib/svc/manifest/application/management/
+    logcmd cp $SRCDIR/files/svc-net-snmp $DESTDIR$PREFIX/lib/svc/method/
+    logcmd chmod +x $DESTDIR$PREFIX/lib/svc/method/svc-net-snmp
+}
+
 init
 download_source $PROG $PROG $VER
 patch_source
 prep_build
 build
-make_isa_stub
+place_smf_files
 fix_permissions
+strip_install
 make_package
 clean_up
 
