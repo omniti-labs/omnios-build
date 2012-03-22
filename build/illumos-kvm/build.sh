d349eaaaea9a3bc762cd01f2887ee1b4fd4b48ed
Eric Sproul <esproul@omniti.com>
2012-03-22 20:19:32 +0000

    tid18365 Illumos KVM packages.  Build both in same script since illumos-kvm-cmd needs the source from illumos-kvm.

--- build/illumos-kvm/build.sh
+++ build/illumos-kvm/build.sh
@@ -3,15 +3,17 @@
 # Load support functions
 . ../../lib/functions.sh
 
+# First we build the kernel module
 PROG=illumos-kvm
-VER=1.0.2
-PVER=0.151002 # Make this match the desired omni-os branch
+VER=1.0.2         # Keep this the same for the utilities build below
+PVER=0.151002     # Make this match the desired omni-os branch
 COMMIT=9621d5228ac4dbdd99cdfe8f2946e7315261a893
 SRC_REPO=https://github.com/joyent/illumos-kvm.git
 KERNEL_SOURCE=/code/omni-os-151002/illumos-omni-os
 PROTO_AREA=$KERNEL_SOURCE/proto
+PATCHDIR=patches.$PROG
 PKG=driver/virtualization/kvm
-SUMMARY="KVM for Illumos (kernel driver)"
+SUMMARY="Illumos KVM kernel driver ($PROG ${COMMIT:0:10})"
 DESC="KVM is the kernel virtual machine, a framework for the in-kernel acceleration of QEMU."
 
 BUILD_DEPENDS_IPS="developer/gcc-3 developer/versioning/git"
@@ -46,6 +48,45 @@ make_prog() {
            PROTO_AREA=$PROTO_AREA \
            GCC=/usr/sfw/bin/gcc || \
         logerr "--- Make failed"
+}
+
+init
+download_source
+patch_source
+prep_build
+build
+fix_permissions
+make_package
+clean_up
+
+# Next, the utilities
+PROG=illumos-kvm-cmd
+VER=1.0.2         # Keep this the same as the kernel driver above
+PVER=0.151002     # Make this match the desired omni-os branch
+COMMIT=099e212e968550ab97f7ba3431e55d9c16a0c78d
+SRC_REPO=https://github.com/joyent/illumos-kvm-cmd.git
+KERNEL_SOURCE=/code/omni-os-151002/illumos-omni-os
+KVM_DIR=$TMPDIR/illumos-kvm-$VER
+PATCHDIR=patches.$PROG
+PKG=system/kvm
+SUMMARY="Illumos KVM utilities ($PROG ${COMMIT:0:10})"
+DESC="KVM is the kernel virtual machine, a framework for the in-kernel acceleration of QEMU."
+
+# Reset a couple of important things
+BUILDDIR=$TMPDIR/$PROG-$VER  # This must be explicitly reset from the run above
+PREFIX=/usr
+
+BUILD_DEPENDS_IPS="developer/gcc-3 developer/versioning/git file/gnu-coreutils"
+
+# Only 64-bit matters
+BUILDARCH=64
+
+make_prog() {
+    CC=/opt/gcc-4.6.2/bin/gcc
+    export KERNEL_SOURCE KVM_DIR PREFIX CC
+    logmsg "--- build.sh"
+    logcmd ./build.sh || \
+        logerr "--- build.sh failed"
 }
 
 init
