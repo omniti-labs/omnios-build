afda73b93f15967321455d37d6c6f9ce3cca2651
Theo Schlossnagle <jesus@omniti.com>
2012-03-25 06:25:07 +0000

    Fix licensing of the build system.

--- build/illumos/build.sh
+++ build/illumos/build.sh
@@ -1,14 +1,34 @@
 #!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+
 SHELL=/usr/bin/bash
 export SHELL
 
-
-
-
 RELEASE_DATE=2012.03.06
-
-
-
 
 # Load support functions
 . ../../lib/functions.sh
@@ -29,7 +49,7 @@ GIT=git
 
 USE_SYSTEM_SSL_HEADERS="TRUE"
 
-PKGSERVER="http://pkg.omniti.com:10006" # jeos repo
+PKGSERVER=$PKGSRVR
 PKGPREFIX=""
 PREFIX=""
 TMPDIR=/code
@@ -75,7 +95,7 @@ clone_source(){
     logmsg "Entering $TMPDIR/$BUILDDIR"
     pushd $TMPDIR/$BUILDDIR > /dev/null 
     logmsg "Cloning OMNI Illumos Source..."
-    logcmd  $GIT clone -b omni src@src.omniti.com:~omni-os/core/illumos-omni-os 
+    logcmd  $GIT clone src@src.omniti.com:~omni-os/core/illumos-omni-os 
     pushd illumos-omni-os 
     ILLUMOS_VERSION="VERSION=\'omni\-os\-`$GIT log --pretty=format:'%h' -n 1`'" 
     echo $ILLUMOS_VERSION
