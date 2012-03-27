10c5f668284bbc8a7cb64edba8f3fc8780d93cce
Theo Schlossnagle <jesus@omniti.com>
2012-03-27 19:17:03 +0000

    branch dep and license for curl

--- build/curl/build.sh
+++ build/curl/build.sh
@@ -29,12 +29,11 @@
 
 PROG=curl       # App name
 VER=7.24.0      # App version
-PVER=1          # Package Version
 PKG=web/curl    # Package name (without prefix)
 SUMMARY="$PROG - command line tool for transferring data with URL syntax"
 DESC="$SUMMARY"
 
-DEPENDS_IPS="web/ca-bundle library/security/openssl@1.0.0 library/zlib"
+DEPENDS_IPS="web/ca-bundle library/security/openssl@1.0.1 library/zlib"
 
 CONFIGURE_OPTS="--enable-thread --with-ca-bundle=/etc/cacert.pem"
 
