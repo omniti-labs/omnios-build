f6ab6b96aeeb3f0a4d338e1ce6b02d873e44db86
Theo Schlossnagle <jesus@omniti.com>
2012-03-29 04:32:40 +0000

    cleanup license and fix branch for rsync

--- build/rsync/build.sh
+++ build/rsync/build.sh
@@ -30,7 +30,6 @@
 PROG=rsync
 VER=3.0.9
 VERHUMAN=$VER
-PVER=0.1
 PKG=network/rsync
 SUMMARY="rsync - faster, flexible replacement for rcp"
 DESC="rsync - faster, flexible replacement for rcp"
@@ -42,7 +41,6 @@ download_source $PROG $PROG $VER
 patch_source
 prep_build
 build
-cp $TMPDIR/$BUILDDIR/COPYING $DESTDIR/LICENSE
 make_isa_stub
 make_package
 clean_up
