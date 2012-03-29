c4136c4e90bfe30aaab0193b4c169b14400305ea
Theo Schlossnagle <jesus@omniti.com>
2012-03-29 04:44:28 +0000

    cleanup licenses and fix branch for freetype

--- build/freetype/build.sh
+++ build/freetype/build.sh
@@ -30,7 +30,6 @@
 PROG=freetype
 VER=2.4.8
 VERHUMAN=$VER
-PVER=0.1
 PKG=system/library/freetype-2
 SUMMARY="FreeType 2 font engine"
 DESC="FreeType 2 font engine"
@@ -43,18 +42,11 @@ CONFIGURE_OPTS="--with-zlib --with-pic --enable-biarch-config --disable-static"
 CONFIGURE_OPTS_32="$CONFIGURE_OPTS_32 --includedir=/usr/include"
 CONFIGURE_OPTS_64="$CONFIGURE_OPTS_64 --includedir=/usr/include"
 
-install_license() {
-    for lic in LICENSE.TXT FTL.TXT GPLv2.TXT
-    do
-        cp $TMPDIR/$BUILDDIR/docs/$lic $DESTDIR/$lic
-    done
-}
 init
 download_source freetype2 $PROG $VER
 patch_source
 prep_build
 build
-install_license
 make_isa_stub
 make_package
 clean_up
