19a225928d388fea61a08a6565ad1000aa2f798b
Zach 'The Mac' Dunn <zdunn@omniti.com>
2012-04-02 19:43:32 +0000

    template doesn't need fix_premissions (we don't have to build as root) and site.sh's URL was incorrect

--- lib/site.sh
+++ lib/site.sh
@@ -1,6 +1,6 @@
 # Package server URL and publisher
 PKGPUBLISHER=ms.omniti.com
-PKGSRVR=http://pkg.omniti.com:10007/
+PKGSRVR=http://pkg-internal.omniti.com:10007/
 
 PREFIX=/opt/omni
 reset_configure_opts
