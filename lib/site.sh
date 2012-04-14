f95e5dbbd6ce6e257a884a1ea99373d40f3679e7
Theo Schlossnagle <jesus@omniti.com>
2012-04-14 16:36:17 -0400

    doc the file repo

--- lib/site.sh
+++ lib/site.sh
@@ -1,3 +1,9 @@
 # Package server URL and publisher
 PKGPUBLISHER=omnios
 PKGSRVR=http://pkg-internal.omniti.com:10006/
+
+# Uncommenting this line will create a on-disk repo in
+# the top level of your checkout and publish there instead
+# of the URI specified above.
+#
+#PKGSRVR=file:///$MYDIR/../tmp.repo/
