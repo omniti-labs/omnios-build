6dff9d2f95557bf4c511f1c574131b773e8de561
Eric Sproul <esproul@omniti.com>
2012-05-13 18:40:43 +0000

    lighttpd 1.4.30

---
+++ build/lighttpd/build.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=lighttpd
+VER=1.4.30
+VERHUMAN=$VER
+PKG=omniti/server/lighttpd
+SUMMARY="$PROG - High-performance, efficient webserver"
+DESC="Security, speed, compliance, and flexibility -- all of these describe lighttpd"
+
+DEPENDS_IPS="system/library/gcc-4-runtime"
+
+PREFIX=/opt/OMNIlighttpd
+reset_configure_opts
+
+BUILDARCH=64
+# Fix for /usr/include/crypt.h syntax error/openssl interaction
+CFLAGS="-DOPENSSL_DISABLE_OLD_DES_SUPPORT"
+LDFLAGS="-lsendfile"
+
+CONFIGURE_OPTS="--with-pic"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
