98119a340a4b92d55bb3454702969696caab71e3
Eric Sproul <esproul@omniti.com>
2012-06-06 21:59:30 +0000

    node modules geoip, iptrie, protobuf. None are yet functional; geoip and iptrie likely need to be updated to work with Node 0.6; protobuf still has compile issues.

---
+++ build/node-iptrie/build.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=node-iptrie
+VER=git
+PKG=omniti/runtime/nodejs/node-iptrie
+SUMMARY="Patricia trees for node, specifically for IPv4 and IPv6 addresses."
+DESC="$SUMMARY"
+
+REPOS=http://github.com/postwait/node-iptrie
+GIT=/usr/bin/git
+
+BUILDARCH=32
+
+PATH=/opt/omni/bin:$PATH
+export PATH
+
+BUILD_DEPENDS_IPS="developer/versioning/git omniti/runtime/nodejs"
+DEPENDS_IPS="omniti/runtime/nodejs"
+
+download_git() {
+    pushd $TMPDIR > /dev/null
+    logmsg "Checking for source directory"
+    if [ -d $BUILDDIR ]; then
+        logmsg "--- removing previous source checkout"
+        logcmd rm -rf $BUILDDIR
+    fi
+    logmsg "Checking code out from git repo"
+    logcmd $GIT clone $REPOS.git $BUILDDIR
+    pushd $BUILDDIR > /dev/null
+    REV=`$GIT log -1  --format=format:%at`
+    REVDATE=`echo $REV | gawk '{ print strftime("%c %Z",$1) }'`
+    VER=0.1.$REV
+    VERHUMAN="checkout from $REVDATE"
+    popd > /dev/null
+    popd > /dev/null
+}
+
+# There is no configuration for this code, so just pretend we did it
+configure32() {
+    true
+}
+
+make_prog() {
+    logmsg "--- make node-geoip"
+    CXX="g++ -L/opt/omni/lib -R/opt/omni/lib" \
+    CXXFLAGS="-DICONV_SRC_CONST=const -I/opt/omni/include" \
+    logcmd /opt/omni/bin/node-waf configure build || \
+        logerr "------ make failed"
+}
+make_install() {
+    logmsg "--- make install"
+    logcmd install -d ${DESTDIR}${PREFIX}/lib/node/ || \
+         logerr "--- Failed to make install directory."
+    for d in lib/iptrie.node ; do
+         if test -e $d ; then 
+             logcmd cp $d ${DESTDIR}$PREFIX/lib/node/ || \
+             logerr "--- Failed to install $d."
+         fi
+    done
+}
+
+init
+download_git
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
---
+++ build/node-protobuf/build.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=node-protobuf
+VER=2.4.0.1
+PKG=omniti/runtime/nodejs/node-protobuf
+SUMMARY="Protocol Buffers for Node.JS"
+DESC="$SUMMARY"
+
+REPOS=https://protobuf-for-node.googlecode.com/hg/ 
+REV=5545aecd5e74
+HG=/usr/bin/hg
+
+BUILDARCH=32
+
+PATH=/opt/omni/bin:$PATH
+export PATH
+
+BUILD_DEPENDS="developer/versioning/mercurial omniti/library/protobuf omniti/runtime/nodejs"
+DEPENDS="omniti/library/protobuf omniti/runtime/nodejs"
+
+download_hg() {
+    pushd $TMPDIR > /dev/null
+    logmsg "Checking for source directory"
+    if [ -d $BUILDDIR ]; then
+        logmsg "--- removing previous source checkout"
+        logcmd rm -rf $BUILDDIR
+    fi
+    logmsg "Checking code out from hg repo"
+    logcmd $HG clone $REPOS $BUILDDIR
+    (cd $BUILDDIR && logcmd $HG checkout 5545aecd5e74)
+    popd > /dev/null
+}
+
+# There is no configuration for this code, so just pretend we did it
+configure32() {
+    true
+}
+
+make_prog() {
+    logmsg "--- make node-geoip"
+    PROTOBUF=/opt/omni LIBDIR=/opt/omni/lib/node \
+    NODE_PATH=/opt/omni/lib/node \
+    CXX="g++ -L/opt/omni/lib -R/opt/omni/lib -R/opt/omni/lib/node" \
+    CXXFLAGS="-DICONV_SRC_CONST=const -I/opt/omni/include -I/opt/omni/include/node/uv-private" \
+    logcmd /opt/omni/bin/node-waf configure || \
+        logerr "------ waf configure failed"
+    logcmd /opt/omni/bin/node-waf build || \
+        logerr "------ waf build failed"
+}
+
+make_install() {
+    logmsg "--- make install"
+    logcmd install -d ${DESTDIR}${PREFIX}/lib/node/ || \
+         logerr "--- Failed to make install directory."
+    DESTDIR=${DESTDIR} logcmd /opt/omni/bin/node-waf install
+}
+
+init
+download_hg
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
