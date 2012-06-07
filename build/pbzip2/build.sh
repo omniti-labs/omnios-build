74e003f8c53fe4cec2af5a7ce63d691946887786
Eric Sproul <esproul@omniti.com>
2012-06-07 17:27:17 +0000

    Parallel compression tools pbzip2 and pigz

---
+++ build/pbzip2/build.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=pbzip2
+VER=1.1.6
+PKG=omniti/compress/pbzip2
+SUMMARY="A parallel implementation of the bzip2 file compressor"
+DESC="$SUMMARY"
+
+# 64-bit only
+BUILDARCH=64
+
+LDFLAGS64="$LDFLAGS64 -L/opt/omni/lib/$ISAPART64 -R/opt/omni/lib/$ISAPART64"
+CFLAGS="-fast -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -KPIC"
+
+# There is no configure here, so just export vars
+configure64() {
+    export CFLAGS="$CFLAGS $CFLAGS64" \
+    CXXFLAGS="$CXXFLAGS $CXXFLAGS64" \
+    CPPFLAGS="$CPPFLAGS $CPPFLAGS64" \
+    LDFLAGS="$LDFLAGS $LDFLAGS64" \
+    CC=$CC CXX=$CXX PREFIX
+}
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
