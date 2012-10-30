83ba7edfcb3a45bbecfb0c5ba2ead5d3f8bdeec0
Theo Schlossnagle <jesus@omniti.com>
2012-10-30 19:53:07 +0000

    update to bloody again

--- lib/site.sh
+++ lib/site.sh
@@ -1,6 +1,6 @@
 # Package server URL and publisher
 PKGPUBLISHER=omnios
-PKGSRVR=http://omnios.int.omniti.net:10002/
+PKGSRVR=http://pkg-il-1.int.omniti.net:10006/
 
 # Uncommenting this line will create a on-disk repo in
 # the top level of your checkout and publish there instead
