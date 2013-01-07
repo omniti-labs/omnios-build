b863a0273a4fdecfd30c8e9d772acf4cd493ce7d
Eric Sproul <esproul@omniti.com>
2013-01-07 22:45:16 +0000

    New package: gimli

---
+++ build/gimli/build.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=gimli
+VER=1.2.1
+VERHUMAN=$VER
+PKG=omniti/developer/debug/gimli
+SUMMARY="gimli is a crash tracing/analysis framework."
+DESC="$SUMMARY"
+
+BUILD_DEPENDS_IPS="developer/build/automake-111"
+
+PATH=/opt/omni/bin:$PATH
+export PATH
+
+run_autogen() {
+    logmsg "Running autogen.sh"
+    pushd $TMPDIR/$BUILDDIR > /dev/null
+    logcmd ./autogen.sh || \
+        logerr "--- failed to run autogen.sh"
+    popd > /dev/null
+}
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+run_autogen
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
