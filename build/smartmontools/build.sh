496ec21285eea76e6fc3c37d7fa1cac09e193d60
Eric Sproul <esproul@omniti.com>
2013-01-13 22:21:55 +0000

    Version bump for smartmontools

--- build/smartmontools/build.sh
+++ build/smartmontools/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=smartmontools
-VER=5.43
+VER=6.0
 VERHUMAN=$VER
 PKG=omniti/system/storage/smartmontools
 SUMMARY="Control and monitor storage systems using SMART"
