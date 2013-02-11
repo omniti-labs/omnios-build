ad92281444ca5513b516ae8a7c35776d5043ec79
Eric Sproul <esproul@omniti.com>
2013-02-11 22:52:37 +0000

    duo_unix two-factor authentication, now with Theo's special sauce.

---
+++ build/duo_unix/build.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=duo_unix
+VER=1.9
+VERHUMAN=$VER
+PKG=omniti/library/security/duo_unix
+SUMMARY="Duo two-factor authentication for Unix systems"
+DESC="Duo provides simple two-factor authentication as a service. This package allows an admin (or ordinary user) to quickly add Duo authentication to any Unix login without setting up secondary user accounts, directory synchronization, servers, or hardware."
+
+BUILD_DEPENDS_IPS="library/security/openssl system/library/gcc-4-runtime"
+DEPENDS_IPS="$BUILD_DEPENDS_IPS"
+
+BUILDARCH=32
+
+# Because PAM stuff has to go in /usr/lib
+PREFIX=/usr
+reset_configure_opts
+
+CONFIGURE_OPTS="--with-pam --disable-static"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
