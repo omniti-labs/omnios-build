8c55199cbfdeef7398ecb1075d6c3f00152d0271
Eric Sproul <esproul@omniti.com>
2013-02-13 14:59:03 +0000

    GNU Global source tagging tool

---
+++ build/global/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=global
+VER=6.2.7
+VERHUMAN=$VER
+PKG=omniti/developer/global
+SUMMARY="GNU Global - a source code tagging system that works the same way across diverse environments"
+DESC="$SUMMARY"
+
+BUILDARCH=32
+LDFLAGS32="-L/usr/gnu/lib -R/usr/gnu/lib"
+
+BUILD_DEPENDS_IPS="library/libtool/libltdl library/ncurses"
+DEPENDS_IPS="library/libtool/libltdl library/ncurses system/library/gcc-4-runtime"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
