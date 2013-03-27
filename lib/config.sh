74221f70d024d6a56224ef0171a8b1421a466e55
Theo Schlossnagle <jesus@omniti.com>
2013-03-27 15:54:21 +0000

    bump (and grind)

--- lib/config.sh
+++ lib/config.sh
@@ -28,7 +28,7 @@
 #############################################################################
 
 # Default branch
-RELVER=151005
+RELVER=151006
 PVER=0.$RELVER
 
 # Which server to fetch files from
