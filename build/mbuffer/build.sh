ec29d1f8e13d3e6d3c3227204e3f57b740ad27bd
Eric Sproul <esproul@omniti.com>
2013-06-19 14:45:48 +0000

    mbuffer IPS build

---
+++ build/mbuffer/build.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=mbuffer
+VER=20130220
+VERHUMAN=$VER
+PKG=omniti/system/mbuffer
+SUMMARY="$PROG - measuring buffer"
+DESC="mbuffer is a tool for buffering data streams"
+
+BUILDARCH=64
+AUTO_DEPENDS=1
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
