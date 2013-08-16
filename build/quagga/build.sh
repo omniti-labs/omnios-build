fd08cb0955667bce098a4f46cd68656c7f2c561b
Chris Nehren <cnehren@omniti.com>
2013-08-16 11:59:15 +0000

    add quagga for bgp and related networking protocols

---
+++ build/quagga/build.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=quagga
+VER=0.99.22.3
+VERHUMAN=$VER
+PKG=networking/quagga
+SUMMARY="quagga software routing suite"
+DESC="Quagga is a routing software suite, providing implementations of OSPFv2, OSPFv3, RIP v1 and v2, RIPng and BGP-4"
+BUILD_DEPENDS_IPS="developer/lexer/flex developer/parser/bison developer/gcc44/libmpfr-gcc44 text/texinfo"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
---
+++ build/quagga/patches/patch-queue.h
@@ -0,0 +1,10 @@
+--- quagga-0.99.22.3/lib/queue.h	Tue Jul 30 14:47:01 2013
++++ quagga-0.99.22.3.fixed/lib/queue.h	Wed Aug 14 23:24:39 2013
+@@ -33,7 +33,6 @@
+ #ifndef _SYS_QUEUE_H_
+ #define	_SYS_QUEUE_H_
+ 
+-#include <sys/cdefs.h>
+ 
+ /*
+  * This file defines four types of data structures: singly-linked lists,
