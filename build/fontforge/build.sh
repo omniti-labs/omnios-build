9b215159f9de98542889d0addd226376880a0556
Zach Malone <zmalone@omniti.com>
2013-08-23 17:41:08 -0400

    Creating fontforge build script

---
+++ build/fontforge/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=fontforge      # App name
+VER=20120731            # App version
+VERHUMAN=$VER-b   # Human-readable version
+#PVER=          # Branch (set in config.sh, override here if needed)
+PKG=omniti/editor            # Package name (e.g. library/foo)
+SUMMARY="An Outline Font Editor"      # One-liner, must be filled in
+DESC="An outline font editor that lets you create your own postscript, truetype, opentype, cid-keyed, multi-master, cff, svg and bitmap (bdf, FON, NFNT) fonts, or edit existing ones. Also lets you convert one format to another."         # Longer description, must be filled in
+DEPENDS_IPS="omniti/runtime/python-26"
+CONFIGURE_OPTS="$CONFIGURE_OPTS
+                --with-python=/opt/python26/"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
