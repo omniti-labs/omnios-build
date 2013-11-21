1ff18ec65fbb4aef046f06eb6fd0381267ecb6ea
Chris Nehren <cnehren@omniti.com>
2013-11-21 19:41:27 +0000

    bump these to gcc48

--- build/binutils/build.sh
+++ build/binutils/build.sh
@@ -34,7 +34,7 @@ PKG=developer/gnu-binutils
 SUMMARY="$PROG -  a collection of binary tools"
 DESC="$SUMMARY"
 
-BUILD_DEPENDS_IPS="gcc47"
+BUILD_DEPENDS_IPS="gcc48"
 [[ "$BUILDARCH" == "both" ]] && BUILDARCH=32
 
 CONFIGURE_OPTS="--enable-gold=yes --exec-prefix=/usr/gnu --program-prefix=g"
