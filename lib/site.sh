7c16fad9fded9910f2bcc36d7fa9065d3b982f5c
Dale Ghent <daleg@omniti.com>
2014-04-15 19:18:15 +0000

    Fix typo and set 151010 repo to rc

--- lib/site.sh
+++ lib/site.sh
@@ -1,6 +1,6 @@
 # Package server URL and publisher
-PKGPUBLISHER?=omnios
-PKGSRVR?=http://pkg-il-1.int.omniti.net:10006/
+: ${PKGPUBLISHER:=omnios}
+: ${PKGSRVR:=http://pkg-il-1.int.omniti.net:10002/}
 
 # To create a on-disk repo in the top level of your checkout
 # and publish there instead of the URI specified above.
