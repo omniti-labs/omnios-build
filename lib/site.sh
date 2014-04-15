2ed306b5f4b21635246073ec0fc1c8d85301c931
Dale Ghent <daleg@omniti.com>
2014-04-15 18:25:23 +0000

    Make PKGPUBLISHER and PKGSRVR conditional, allowing one to set those varables in their shell environment and preclude the need to edit lib/site.sh directly.

--- lib/site.sh
+++ lib/site.sh
@@ -1,9 +1,8 @@
 # Package server URL and publisher
-PKGPUBLISHER=omnios
-PKGSRVR=http://omnios.int.omniti.net:10002/
+PKGPUBLISHER?=omnios
+PKGSRVR?=http://omnios.int.omniti.net:10002/
 
-# Uncommenting this line will create a on-disk repo in
-# the top level of your checkout and publish there instead
-# of the URI specified above.
+# To create a on-disk repo in the top level of your checkout
+# and publish there instead of the URI specified above.
 #
 #PKGSRVR=file:///$MYDIR/../tmp.repo/
