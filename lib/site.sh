35562f6677a41c627f55653b15ce542ff78bf821
Dan McDonald <danmcd@omniti.com>
2014-06-01 17:17:32 -0400

    Update KVM_ROLLBACK value, and document it further.

--- lib/site.sh
+++ lib/site.sh
@@ -14,5 +14,8 @@
 
 # These two should be uncommented and set to specific git changeset IDs
 # if illumos-kvm and illumos-kvm-cmd get too far ahead of illumos-{gate,omnios}.
-#KVM_ROLLBACK=a8ea37e8deb99265682c66c23f787f704e77fb91
+# NOTE -> These two values reflect the current known-to-work revisions.
+# If a revision matches tip, you don't need to uncomment it.  If it is behind
+# tip, you MUST uncomment it, or KVM/KVM-cmd won't build.
+#KVM_ROLLBACK=43aa6602f0d68ff7e032aad06645e34e9921d976
 #KVM_CMD_ROLLBACK=1c6181be55d1cadc4426069960688307a6083131
