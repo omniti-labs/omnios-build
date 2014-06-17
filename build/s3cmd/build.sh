96d8d24a208f420f41bb46cd99b88684aa60e953
mmurphy <mmurphy@omniti.com>
2014-06-17 21:50:01 +0000

    tid25605 adding build script for s3cmd

---
+++ build/s3cmd/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright @@CYEAR@@ OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=s3cmd
+VER=1.5.0 #-beta1
+VERHUMAN=$VER   # Human readable version
+PKG=omniti/database/s3cmd
+SUMMARY="Command line tool for managing Amazon S3 and CloudFront services"      # Change this
+DESC="Command line tool for managing Amazon S3 and CloudFront services"         # Change this
+
+BUILD_DEPENDS_IPS="omniti/runtime/python-26"
+DEPENDS_IPS="omniti/runtime/python-26"
+
+LDFLAGS64="-L$PYTHONLIB -R$PYTHONLIB -L/opt/omni/lib/$ISAPART64 -R/opt/omni/lib/$ISAPART64"
+
+
+# omniti-ms python is 64-bit only
+BUILDARCH=64
+PYTHON=/opt/python26/bin/python
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+python_build
+make_package
+clean_up
