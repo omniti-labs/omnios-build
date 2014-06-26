aa1b916a4c495d217255da5a9268849d1d2eaedb
Dan McDonald <danmcd@omniti.com>
2014-06-25 22:14:30 -0400

    ISC DHCP now has SMF service properties like OI and Oracle Solaris.

--- build/isc-dhcp/build.sh
+++ build/isc-dhcp/build.sh
@@ -28,21 +28,29 @@ DESC="$SUMMARY $VER"
 
 DEPENDS_IPS="system/library"
 
+# XXX 32-bit until Y2038 rears its ugly head.
 BUILDARCH=32
 
 CONFIGURE_OPTS="--enable-use-sockets --enable-ipv4-pktinfo --prefix=$PREFIX --bindir=$PREFIX/bin --sbindir=$PREFIX/sbin"
 
 pre_package() {
     # Make directories and install extra files before package construction.
-    logcmd mkdir -p $DESTDIR/usr/share/isc-dhcp/examples || \ logerr
+    logcmd mkdir -p $DESTDIR/usr/share/isc-dhcp/examples || logerr \
 	"mkdir of $DESTDIR/usr/share/isc-dhcp/examples failed"
-    logcmd mkdir -p $DESTDIR/lib/svc/manifest/network || \ logerr
+    logcmd mkdir -p $DESTDIR/lib/svc/manifest/network || logerr \
 	"mkdir of $DESTDIR/lib/svc/manifest/network failed"
+    logcmd mkdir -p $DESTDIR/lib/svc/method || logerr \
+	"mkdir of $DESTDIR/lib/svc/method failed"
+    logcmd mkdir -p $DESTDIR/var/db || logerr "mkdir of $DESTDIR/var/db failed"
     logcmd mkdir -p $DESTDIR/var/db || logerr "mkdir of $DESTDIR/var/db failed"
     logcmd touch $DESTDIR/var/db/dhcpd.leases
     logcmd touch $DESTDIR/var/db/dhcpd6.leases
     logcmd cp $SRCDIR/files/isc-dhcp.xml $DESTDIR/lib/svc/manifest/network || \
 	logerr "Cannot copy in manifest."
+    chmod 0444 $DESTDIR/lib/svc/manifest/network/isc-dhcp.xml
+    logcmd cp $SRCDIR/files/dhcrelay $DESTDIR/lib/svc/method || \
+	logerr "Cannot copy in method."
+    chmod 0555 $DESTDIR/lib/svc/method/dhcrelay
 }
 
 init
