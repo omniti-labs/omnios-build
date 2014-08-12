4efdbea26f4ded30e891429d4492710c0ee758da
Dan McDonald <danmcd@omniti.com>
2014-08-12 14:29:31 -0400

    Update coreutils to 8.23

--- build/coreutils/build.sh
+++ build/coreutils/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=coreutils          # App name
-VER=8.22                # App version
+VER=8.23                # App version
 PKG=file/gnu-coreutils  # Package name (without prefix)
 SUMMARY="coreutils - GNU core utilities"
 DESC="GNU core utilities ($VER)"
