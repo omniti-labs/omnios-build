f4243eb74517f2b4c457bbdd38574bca90e39977
Eric Sproul <esproul@omniti.com>
2014-08-15 16:49:54 -0400

    Pull in the feature that allows for auto-creating a local file-based repo

--- lib/site.sh
+++ lib/site.sh
@@ -2,6 +2,11 @@
 : ${PKGPUBLISHER=ms.omniti.com}
 : ${PKGSRVR=http://pkg-il-1.int.omniti.net:10007/}
 
+# To create a on-disk repo in the top level of your checkout
+# and publish there instead of the URI specified above.
+#
+#PKGSRVR=file:///$MYDIR/../tmp.repo/
+
 export PATH=$PATH:/opt/omni/bin
 
 PREFIX=/opt/omni
