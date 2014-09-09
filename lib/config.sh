92e846feafa7046bee65f205c23b49de38133bba
Dan McDonald <danmcd@omniti.com>
2014-09-09 05:19:14 +0000

    Bump personality for OmniOS r151012 (new Stable)

--- lib/config.sh
+++ lib/config.sh
@@ -28,7 +28,7 @@
 #############################################################################
 
 # Default branch
-RELVER=151011
+RELVER=151012
 PVER=0.$RELVER
 
 # Which server to fetch files from.
