ee0817cb4fc702ecec194f4a1f0e879ed0d6bf0c
Dan McDonald <danmcd@omniti.com>
2014-09-24 16:01:56 -0400

    Forgot to update pkg patch level in bash

--- build/bash/build.sh
+++ build/bash/build.sh
@@ -41,7 +41,7 @@
 
 PROG=bash       # App name
 VER=4.2         # App version
-PATCHLEVEL=45   # Patch level
+PATCHLEVEL=48   # Patch level
 VERHUMAN="$VER patchlevel $PATCHLEVEL"
 PKG=shell/bash  # Package name (without prefix)
 SUMMARY="GNU Bourne-Again shell (bash)"
