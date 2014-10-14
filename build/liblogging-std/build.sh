258a83887f6eb1734b356e9693916e8114d69515
Colin <colin@omniti.com>
2014-10-14 17:16:33 +0000

    Adding build script for liblogging-stdlog

---
+++ build/liblogging-std/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=liblogging
+VER=1.0.4
+VERHUMAN=$VER
+PKG=omniti/library/liblogging-stdlog
+SUMMARY="An easy to use and lightweight signal-safe logging library"
+DESC="$SUMMARY"
+
+BUILD_DEPENDS_IPS="omniti/library/pkgconf" 
+
+CONFIGURE_OPTS="--enable-stdlog=yes --enable-journal=no --enable-rfc3195=no --disable-man-pages"
+
+init
+download_source $PROG $PROG ${VER}
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
