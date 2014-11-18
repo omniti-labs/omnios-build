2125489e6c96ad10594016d5ad3681a9886b4f65
Dan McDonald <danmcd@omniti.com>
2014-11-17 19:47:00 -0500

    Add intltool to allow proper building of pkg(5).

---
+++ build/intltool/build.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+#
+# This file and its contents are supplied under the terms of the
+# Common Development and Distribution License ("CDDL"), version 1.0.
+# You may only use this file in accordance with the terms of version
+# 1.0 of the CDDL.
+#
+# A full copy of the text of the CDDL should have accompanied this
+# source.  A copy of the CDDL is also available via the Internet at
+# http://www.illumos.org/license/CDDL.
+#
+# Copyright 2014 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=intltool
+VER=0.50.2
+VERHUMAN=$VER
+PKG=text/intltool
+SUMMARY="Extracts translatable strings from specific source file types."
+DESC="$SUMMARY $VER"
+
+DEPENDS_IPS="system/library SUNWcs"
+
+BUILDARCH=32
+
+CONFIGURE_OPTS="--bindir=$PREFIX/bin"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
