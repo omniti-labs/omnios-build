1977b2c316c798901b49ebf97fe7cd5601e6fdb9
Dan McDonald <danmcd@omniti.com>
2015-01-15 20:38:26 +0000

    Update curl to 7.40.0

--- build/curl/build.sh
+++ build/curl/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=curl       # App name
-VER=7.39.0      # App version
+VER=7.40.0      # App version
 PKG=web/curl    # Package name (without prefix)
 SUMMARY="$PROG - command line tool for transferring data with URL syntax"
 DESC="$SUMMARY"
