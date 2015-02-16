f3d6d48c08b73b49426a072a2ac235bf6d261c2c
Dan McDonald <danmcd@omniti.com>
2015-02-16 16:24:31 -0500

    Update git to 2.3.0

--- build/git/build.sh
+++ build/git/build.sh
@@ -29,7 +29,7 @@
 . ../../lib/functions.sh
 
 PROG=git
-VER=2.2.1
+VER=2.3.0
 PKG=developer/versioning/git
 SUMMARY="$PROG - a free and open source, distributed version control system"
 DESC="$SUMMARY"
