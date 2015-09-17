f7cc803c01b2c27ae586f26490b2ba9a8e4a9c75
Dan McDonald <danmcd@omniti.com>
2015-09-17 17:09:26 -0400

    Update Mercurial to 3.5.1

--- build/mercurial/build.sh
+++ build/mercurial/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=mercurial
-VER=2.7.1
+VER=3.5.1
 PKG=developer/versioning/mercurial
 SUMMARY="$PROG - a free and open source, distributed version control system"
 DESC="$SUMMARY"
