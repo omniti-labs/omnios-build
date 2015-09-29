3fa15f32758a2bcd782855b1f8328b43252629a0
Dan McDonald <danmcd@omniti.com>
2015-09-29 13:48:53 -0400

    Update bash to 4.3p42

--- build/bash/build.sh
+++ build/bash/build.sh
@@ -43,7 +43,7 @@
 
 PROG=bash       # App name
 VER=4.3         # App version
-PATCHLEVEL=33   # Patch level
+PATCHLEVEL=42   # Patch level
 VERHUMAN="$VER patchlevel $PATCHLEVEL"
 PKG=shell/bash  # Package name (without prefix)
 SUMMARY="GNU Bourne-Again shell (bash)"
