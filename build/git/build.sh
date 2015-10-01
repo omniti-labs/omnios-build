c15ee737dac934d313bd4499b176c2321ff9ed21
Dan McDonald <danmcd@omniti.com>
2015-10-01 16:56:23 -0400

    Update git to 2.6.1

--- build/git/build.sh
+++ build/git/build.sh
@@ -21,7 +21,7 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2011-2015 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 # Copyright (c) 2014 by Delphix. All rights reserved.
 #
@@ -29,7 +29,7 @@
 . ../../lib/functions.sh
 
 PROG=git
-VER=2.4.5
+VER=2.6.1
 PKG=developer/versioning/git
 SUMMARY="$PROG - a free and open source, distributed version control system"
 DESC="$SUMMARY"
