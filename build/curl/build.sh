ccc2d55140028868986e3cc07ab7a2c593b196a7
Dan McDonald <danmcd@omniti.com>
2015-10-01 14:48:40 -0400

    Update curl to 7.44.0

--- build/curl/build.sh
+++ build/curl/build.sh
@@ -21,14 +21,14 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2011-2015 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=curl       # App name
-VER=7.42.1      # App version
+VER=7.44.0      # App version
 PKG=web/curl    # Package name (without prefix)
 SUMMARY="$PROG - command line tool for transferring data with URL syntax"
 DESC="$SUMMARY"
