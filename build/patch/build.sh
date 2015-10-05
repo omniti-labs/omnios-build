a2d24912246219b3bbde321edfee08c41c33ce7d
Dan McDonald <danmcd@omniti.com>
2015-10-05 16:26:33 -0400

    Update gnu-patch to 2.7.5

--- build/patch/build.sh
+++ build/patch/build.sh
@@ -21,14 +21,14 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2011-2015 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=patch
-VER=2.7.4
+VER=2.7.5
 PKG=text/gnu-patch
 SUMMARY="The GNU Patch utility"
 DESC="$SUMMARY"
