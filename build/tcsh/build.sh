a84552bc7ea57d08eacb4274cff5ca0e98a6fd03
Dan McDonald <danmcd@omniti.com>
2015-10-06 19:25:15 -0400

    Update tcsh to 6.19.0

--- build/tcsh/build.sh
+++ build/tcsh/build.sh
@@ -21,14 +21,14 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2011-2015 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=tcsh
-VER=6.18.01
+VER=6.19.00
 VERHUMAN=$VER
 PKG=shell/tcsh
 SUMMARY="Tenex C-shell (tcsh)"
