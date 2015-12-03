40d76f937100fe8a03070ec5f22982c151a71f3c
Dan McDonald <danmcd@omniti.com>
2015-12-03 15:19:25 -0500

    Update wget to 1.16.3

--- build/wget/build.sh
+++ build/wget/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=wget       # App name
-VER=1.16        # App version
+VER=1.16.3      # App version
 VERHUMAN=$VER   # Human-readable version
 PKG=web/wget    # Package name (without prefix)
 SUMMARY="$PROG - a utility to retrieve files from the World Wide Web"
