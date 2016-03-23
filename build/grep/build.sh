62beaffc2e45220eff68e1f9dc43bee1f6e44af6
Dan McDonald <danmcd@omniti.com>
2016-03-23 11:44:02 -0400

    Update gnu-grep to 2.24

--- build/grep/build.sh
+++ build/grep/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=grep       # App name
-VER=2.21         # App version
+VER=2.24         # App version
 PKG=text/gnu-grep    # Package name (without prefix)
 SUMMARY="ggrep - GNU grep utilities"
 DESC="$SUMMARY $VER"
