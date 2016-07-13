8bc13e6660a2ca646e734ac703b1091d81d1b336
Dale Ghent <daleg@omniti.com>
2016-07-13 17:40:17 -0400

    mozilla-nss: security bump to 3.25

--- build/mozilla-nss-nspr/build.sh
+++ build/mozilla-nss-nspr/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=nss
-VER=3.23
+VER=3.25
 # Include NSPR version since we're downloading a combined tarball.
 NSPRVER=4.12
 # But set BUILDDIR to just be the NSS version.
