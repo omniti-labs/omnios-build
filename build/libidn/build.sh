4ef8b549441180db9086ce3362477a3a1a2bec30
Dan McDonald <danmcd@omniti.com>
2016-07-20 20:21:57 -0400

    Update libidn to 1.33

--- build/libidn/build.sh
+++ build/libidn/build.sh
@@ -21,14 +21,14 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=libidn
-VER=1.32
+VER=1.33
 VERHUMAN=$VER
 PKG=library/libidn
 SUMMARY="The Internationalized Domains Library"
