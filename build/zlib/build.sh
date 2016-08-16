eab78cb31784bce3d6c60a969950e7638d67021c
Dan McDonald <danmcd@omniti.com>
2016-08-16 13:56:08 -0400

    Incorrect symlink creation in zlib

--- build/zlib/build.sh
+++ build/zlib/build.sh
@@ -44,7 +44,7 @@ make_prog64() {
 # Relocate the libs to /lib, to match upstream
 move_libs() {
     logcmd mkdir -p $DESTDIR/lib/amd64
-    logcmd ln -s $DESTDIR/lib/64 amd64
+    logcmd ln -s amd64 $DESTDIR/lib/64
     logcmd mv $DESTDIR/usr/lib/lib* $DESTDIR/lib || \
         logerr "failed to move libs (32-bit)"
     logcmd mv $DESTDIR/usr/lib/amd64/lib* $DESTDIR/lib/amd64 || \
