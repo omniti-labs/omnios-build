4c16fd7558edc7bde0c9843f0568a2b5059c619f
Dan McDonald <danmcd@omniti.com>
2016-09-22 16:05:35 -0400

    Update dbus to 1.11.4

--- build/dbus/build.sh
+++ build/dbus/build.sh
@@ -21,14 +21,14 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=dbus
-VER=1.10.8
+VER=1.11.4
 PKG=dbus ##IGNORE##
 SUMMARY="$PROG - IPC-based message notifications"
 DESC="$SUMMARY"
