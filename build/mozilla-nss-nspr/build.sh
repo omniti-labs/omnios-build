39e1d344e7d0b1f681d90dc5a4b8b3c8e80f5534
Dan McDonald <danmcd@omniti.com>
2016-09-22 15:25:13 -0400

    Update NSS to 3.26, and the ca-bundle.

--- build/mozilla-nss-nspr/build.sh
+++ build/mozilla-nss-nspr/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=nss
-VER=3.25
+VER=3.26
 # Include NSPR version since we're downloading a combined tarball.
 NSPRVER=4.12
 # But set BUILDDIR to just be the NSS version.
