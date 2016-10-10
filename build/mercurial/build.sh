d1214b12187005cd1f181a3c3af0143065e6435a
Dan McDonald <danmcd@omniti.com>
2016-10-10 16:18:21 -0400

    Update Mercurial to 3.9.2

--- build/mercurial/build.sh
+++ build/mercurial/build.sh
@@ -21,14 +21,14 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=mercurial
-VER=3.7.3
+VER=3.9.2
 PKG=developer/versioning/mercurial
 SUMMARY="$PROG - a free and open source, distributed version control system"
 DESC="$SUMMARY"
