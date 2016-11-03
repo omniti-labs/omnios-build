2ff6590fc4cfc713bde04e7a72833b874b05df56
Dale Ghent <daleg@omniti.com>
2016-11-03 04:59:28 +0000

    openssh: Add OpenSSH 7.2p2 package

--- build/openssh/build.sh
+++ build/openssh/build.sh
@@ -21,20 +21,18 @@
 # CDDL HEADER END
 #
 #
-# Copyright 2011-2013 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2015 OmniTI Computer Consulting, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 # Load support functions
 . ../../lib/functions.sh
 
 PROG=openssh
-VER=6.1p1
+VER=7.2p2
 VERHUMAN=$VER
 PKG=network/openssh
 SUMMARY="OpenSSH Client and utilities"
 DESC="OpenSSH Secure Shell protocol Client and associated Utilities"
-
-GLOBAL_DEPENDS="library/security/openssl@1.0.1 library/zlib@1.2 system/library system/library/g++-4-runtime@4.7.2"
 
 BUILDARCH=32
 # Since we're only building 32-bit, don't bother with isaexec subdirs
@@ -49,36 +47,55 @@ CONFIGURE_OPTS_32="
     "
 # Feature choices
 CONFIGURE_OPTS="
-    --with-solaris-contracts
-    --with-solaris-projects
-    --with-tcp-wrappers
-    --with-ssl-engine
+    --with-audit=solaris
+    --with-kerberos5=$PREFIX/usr
     --with-pam
+    --with-sandbox=solaris
+    --with-solaris-contracts
+    --with-solaris-privs
+    --with-tcp-wrappers
+    --with-4in6
+    --enable-strip=no
+    --without-rpath
+    --disable-lastlog
+    --with-privsep-user=daemon
+    --with-ssl-engine
+    --with-solaris-projects
     "
 
-install_smf() {
-    logmsg "Installing SMF components"
-    logcmd mkdir -p $DESTDIR/lib/svc/manifest/network || \
-        logerr "--- Failed to create manifest directory"
-    logcmd cp $SRCDIR/ssh.xml $DESTDIR/lib/svc/manifest/network/ || \
-        logerr "--- Failed to copy manifest file"
-    logcmd mkdir -p $DESTDIR/lib/svc/method || \
-        logerr "--- Failed to create method directory"
-    logcmd cp $SRCDIR/method-sshd $DESTDIR/lib/svc/method/sshd || \
-        logerr "--- Failed to copy method script"
+CFLAGS+="-DPAM_ENHANCEMENT -DSET_USE_PAM -DPAM_BUGFIX -DDTRACE_SFTP "
+CFLAGS+="-I/usr/include/kerberosv5 -DKRB5_BUILD_FIX -DDISABLE_BANNER "
+CFLAGS+="-DDEPRECATE_SUNSSH_OPT -DOPTION_DEFAULT_VALUE -DSANDBOX_SOLARIS"
+
+auto_reconf() {
+        # This package needs a whack upside the head post-patches!
+        pushd $TMPDIR/$BUILDDIR
+        autoreconf -fi
+        popd
+}
+
+copy_smf() {
+    mkdir -p $TMPDIR/$BUILDDIR/smf
+    cp $SRCDIR/files/sshd-method $TMPDIR/$BUILDDIR/smf/method.sh ||
+        logerr 'method script copy failed'
+    cp $SRCDIR/files/ssh.xml $TMPDIR/$BUILDDIR/smf/manifest.xml ||
+        logerr 'manifest copy failed'
 }
 
 init
 download_source $PROG $PROG $VER
 patch_source
+copy_smf
+auto_reconf
 prep_build
+run_autoconf
 build
 
 # Remove the letter from VER for packaging
 VER=${VER//p/.}
 
 # Client package
-DEPENDS_IPS="-network/ssh -network/ssh/ssh-key $GLOBAL_DEPENDS"
+RUN_DEPENDS_IPS="-pkg:/network/ssh -pkg:/network/ssh/ssh-key"
 make_package client.mog
 
 # Server package
@@ -86,8 +103,7 @@ PKG=network/openssh-server
 PKGE=$(url_encode $PKG)
 SUMMARY="OpenSSH Server"
 DESC="OpenSSH Secure Shell protocol Server"
-DEPENDS_IPS="-service/network/ssh network/openssh $GLOBAL_DEPENDS"
-install_smf
+RUN_DEPENDS_IPS="-pkg:/service/network/ssh pkg:/network/openssh@$VER"
 make_package server.mog
 
 clean_up
