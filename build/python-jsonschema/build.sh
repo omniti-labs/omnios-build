37c54a0d198431a6ac3530d7d407c193db9e5b80
Dan McDonald <danmcd@omniti.com>
2016-12-14 12:46:51 -0500

    Add jsonschema for Python2.7

---
+++ build/python-jsonschema/build.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2016 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=jsonschema
+VER=2.5.1
+SUMMARY="An implementation of JSON Schema validation for Python"
+DESC="$SUMMARY"
+
+# Pardon the copy/paste, but we have to do this for 2.7 for now.
+# And the only way buildctl detects packages is by grepping for PKG assignment.
+
+OLDPV=$PYTHONVER
+
+set_python_version 2.7
+XFORM_ARGS="-D PYTHONVER=$PYTHONVER"
+PKG=library/python-2/jsonschema-27
+RUN_DEPENDS_IPS="runtime/python-27"
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+python_build
+make_package
+clean_up
+
+set_python_version $OLDPV
