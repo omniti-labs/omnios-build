f846b125c01c3a2e50da3664ac5ddfa3b252d33a
HummyPkg <af123@hummypkg.org.uk>
2017-05-30 09:39:21 +0000

    update openssl to 1.0.2l

--- build/openssl/build.sh
+++ build/openssl/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=openssl
-VER=1.0.2k
+VER=1.0.2l
 VERHUMAN=$VER
 PKG=library/security/openssl # Package name (without prefix)
 SUMMARY="$PROG - A toolkit for Secure Sockets Layer (SSL v2/v3) and Transport Layer (TLS v1) protocols and general purpose cryptographic library"
