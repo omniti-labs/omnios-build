86c94633dab69b9d868947d9eb39fa78e2f7b543
mmurphy <mmurphy@omniti.com>
2018-08-22 19:59:06 +0000

    version updates for curl, git, openssl, wget

--- build/git/build.sh
+++ build/git/build.sh
@@ -29,7 +29,7 @@
 . ../../lib/functions.sh
 
 PROG=git
-VER=2.14.2
+VER=2.14.4
 PKG=developer/versioning/git
 SUMMARY="$PROG - a free and open source, distributed version control system"
 DESC="$SUMMARY"
